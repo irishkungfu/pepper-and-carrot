148a455e70d7d9bf4e49b85a345874805b2acfb9
Jared Gilpin <irishkungfu@localhost.localdomain>
2018-02-18 18:24:40 -0600

    add logic for mechanics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,123 @@
-$( document ).ready(function() {
-    var character = {
-        id: null,
-        name: null,
-        healthPoints: null,
-        attackPower: null,
-        counterAttack: null,
-        image: null,
-        state: null,
-        attackPowerIncrease: function () {
-            this.attackPower = this.attackPower += 6;
-            return;
-        }
-    }
-    
-        var lukeSkywalker = Object.create(character);
-        lukeSkywalker.name = 'test';
-        lukeSkywalker.attackPower = 6;
-        lukeSkywalker.attackPowerIncrease();
-        console.log(lukeSkywalker);
+var phase = 'select_character'; // Select Character, Attack, Select Defender
+var characterCount = 4;
+var attacker;
+var defender;
+characterList = ['pepper', 'thyme', 'sage', 'cumin'];
+var defenderCounter = 3;
 
-    
-    // console.log(lukeSkywalker);
-    // var luke_skywalker = Object.create(characters);
-    // luke_skywalker.id = 'Luke Skywalker';
-    // console.log(luke_skywalker.id);
 
+
+var pepper = {
+    id: 'pepper',
+    name: 'Pepper',
+    counterAttack: 8,
+    hp: 120,
+    attackPower: 8,
+    incrementAttack: function () { this.attackPower++ },
+}
+var thyme = {
+    id: 'thyme',
+    name: 'Thyme',
+    counterAttack: 8,
+    hp: 1800,
+    attackPower: 100,
+    incrementAttack: function () { this.attackPower++ },
+}
+
+var sage = {
+    id: 'sage',
+    name: 'The Sage',
+    counterAttack: 8,
+    hp: 150,
+    attackPower: 20,
+    incrementAttack: function () { this.attackPower++ },
+}
+var cumin = {
+    id: 'cumin',
+    name: 'Cumin',
+    counterAttack: 8,
+    hp: 150,
+    attackPower: 20,
+    incrementAttack: function () { this.attackPower++ },
+}
+$(function (){
+    $('#pepper_hp').text('HP: ' + pepper.hp);
+    $('#sage_hp').text('HP: ' + sage.hp);
+    $('#cumin_hp').text('HP: ' + cumin.hp);
+    $('#thyme_hp').text('HP: ' + thyme.hp);
 });
 
-var add = (function () {
-    var counter = 0;
-    return function () {return counter += 1;}
-})();
+$( document ).ready(function() {
+    
+    $('.character-wrapper').click(function () {
+        var area = $(this).parent().prop('id'); // Identify which section you are in
+        // var getHtml;
+        console.log('phase = ' + phase);
+        character = $(this).children().prop('id');
 
-add();
-add();
-add();
+    
+        if (area === 'select-your-character_area') {
+            $(this).parent().prop('id', 'select-defender_area')
 
-console.log(add());
+            var getHtml = $(this).children().html();
+            console.log(getHtml)
 
-function Movie(title, stub, video) {
-    this.title = title;
-    this.stub = stub;
-    this.video = video;
-};
+            $('#attacker_area').children().children().children().replaceWith(getHtml);
+            $(this).parent().prop('id', 'select-defender_area')
+            phase = 'select_defender';
 
-var the_wizard_of_oz = Movie('The Wizard of Oz','asdf','assets/videos/the_wizard_of_oz.mp4');
-console.log(the_wizard_of_oz);
+            characterCount--;
+            attacker = eval(character);
+            console.log('attacker = ' + attacker.name);
+            $(this).remove();
+            console.log(characterCount);
+
+            
+        } 
+        else if (area === 'select-defender_area' && phase === 'select_defender') {
+            var getHtml = $(this).children().html();
+
+            $('#defender_area').children().children().children().replaceWith(getHtml);
+            $(this).parent().prop('id', 'select-defender_area')
+
+            characterCount--;
+
+            if (characterCount === 0) {
+                $(this).parent().hide(1000);
+            }
+            phase = 'attack';
+            defender = eval(character);
+            console.log('defender = ' + defender.name);
+            console.log(characterCount);
+            $(this).remove();
+        }
+    });
+    $('#attack_button').click(function () {
+        if (phase === 'attack') {
+            var attackerHpId = '#' + attacker.id + "_hp";
+            var defenderHpId = '#' + defender.id + "_hp";
+
+            console.log('attack phase is operational');
+
+            attacker.hp = attacker.hp - defender.counterAttack;
+            $(attackerHpId).text(attacker.hp);
+            defender.hp = defender.hp - attacker.attackPower;
+            $(defenderHpId).text(defender.hp);
+
+            if (attacker.hp <= 0 ) {
+                $('#battle-status').text('you lost!')
+                phase = 'lost';
+                console.log('works');
+                // add in reset button --thinking change the button id and run through different click event
+            }
+            if (defender.hp <= 0) {
+                $('#battle-status').text('You have slain ' + defender.name + '!')
+                phase = "select_defender";
+                defenderCounter--;
+                if (defenderCounter === 0) {
+                    $('#battle-status').text('You have have defeated all the other witches!')
+                }
+            }
+        }
+    });
+});
